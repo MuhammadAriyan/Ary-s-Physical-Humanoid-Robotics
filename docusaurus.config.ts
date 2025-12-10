e69fec81f1cd70fe9f25081442b97fecf20da075
Aryan <aryan4march@gmail.com>
2025-12-10 21:49:42 +0500

    Fix baseUrl configuration for Docusaurus site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: 'https://MuhammadAriyan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Humanoid-Robotics-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
